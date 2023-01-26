adcda39a3c08a9519e3c42dd2cd302f5ff103500
fjs9 <121412941+fjs9@users.noreply.github.com>
2023-01-26 22:51:37 +0100

    główny listener enetów

---
+++ event/main_listener.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ *
+ * Gmerk. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2023, FJS
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace fjs9\gmerk\event;
+
+/**
+ * @ignore
+ */
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Gmerk Event listener.
+ */
+class main_listener implements EventSubscriberInterface
+{
+	public static function getSubscribedEvents()
+	{
+		return [
+			'core.user_setup'							=> 'load_language_on_setup',
+			'core.page_header'							=> 'add_page_header_link',
+			'core.viewonline_overwrite_location'		=> 'viewonline_page',
+	'core.display_forums_modify_template_vars'	=> 'display_forums_modify_template_vars',
+		];
+	}
+
+	/* @var \phpbb\language\language */
+	protected $language;
+
+	/* @var \phpbb\controller\helper */
+	protected $helper;
+
+	/* @var \phpbb\template\template */
+	protected $template;
+
+	/** @var string phpEx */
+	protected $php_ext;
+
+	/**
+	 * Constructor
+	 *
+	 * @param \phpbb\language\language	$language	Language object
+	 * @param \phpbb\controller\helper	$helper		Controller helper object
+	 * @param \phpbb\template\template	$template	Template object
+	 * @param string                    $php_ext    phpEx
+	 */
+	public function __construct(\phpbb\language\language $language, \phpbb\controller\helper $helper, \phpbb\template\template $template, $php_ext)
+	{
+		$this->language = $language;
+		$this->helper   = $helper;
+		$this->template = $template;
+		$this->php_ext  = $php_ext;
+	}
+
+	/**
+	 * Load common language files during user setup
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = [
+			'ext_name' => 'fjs9/gmerk',
+			'lang_set' => 'common',
+		];
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	/**
+	 * Add a link to the controller in the forum navbar
+	 */
+	public function add_page_header_link()
+	{
+		$this->template->assign_vars([
+			'U_GMERK_PAGE'	=> $this->helper->route('fjs9_gmerk_controller', ['name' => 'world']),
+		]);
+	}
+
+	/**
+	 * Show users viewing Gmerk page on the Who Is Online page
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function viewonline_page($event)
+	{
+		if ($event['on_page'][1] === 'app' && strrpos($event['row']['session_page'], 'app.' . $this->php_ext . '/demo') === 0)
+		{
+			$event['location'] = $this->language->lang('VIEWING_FJS9_GMERK');
+			$event['location_url'] = $this->helper->route('fjs9_gmerk_controller', ['name' => 'world']);
+		}
+	}
+
+	/**
+	 * A sample PHP event
+	 * Modifies the names of the forums on index
+	 *
+	 * @param \phpbb\event\data	$event	Event object
+	 */
+	public function display_forums_modify_template_vars($event)
+	{
+		$forum_row = $event['forum_row'];
+		$forum_row['FORUM_NAME'] .= $this->language->lang('GMERK_EVENT');
+		$event['forum_row'] = $forum_row;
+	}
+}
